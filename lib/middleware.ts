ce6d083e5285c9db940dc6d3fa6579b52df6c16a
Hyunje Alex Jun <me@noraesae.net>
2015-11-24 01:07:37 +0900

    Add options
    Please refer to form API in the following page.
    
    https://github.com/felixge/node-formidable#api

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -3,14 +3,28 @@ import _ = require('underscore');
 import express = require('express');
 import formidable = require('formidable');
 
-export interface IOption { }
+export interface IOption {
+  encoding?: string;
+  uploadDir?: string;
+  keepExtensions?: boolean;
+  maxFieldsSize?: number;
+  maxFields?: number;
+  hash?: boolean;
+  multiples?: boolean;
+}
+
 export type Middleware = (option?: IOption) => express.RequestHandler;
 
-export const parse: Middleware = () => {
+export const parse: Middleware = (option = {}) => {
   return (req, res, next) => {
     let contentType = req.headers['content-type'];
     if (contentType && contentType.indexOf('multipart/form-data') >= 0) {
       let form = new formidable.IncomingForm();
+
+      Object.keys(option).forEach(optKey => {
+        form[optKey] = option[optKey];
+      });
+
       let fields: {[name: string]: any} = {};
 
       function setOrAppend(name: string, value: any) {
