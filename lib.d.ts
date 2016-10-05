c1e51e9afbdaf183c86874e667ecae75053d4d32
Jun <me@noraesae.net>
2016-10-05 23:43:43 +0900

    Remove ts dependencies

--- lib.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="lib.d/express/express.d.ts" />
-/// <reference path="lib.d/formidable/formidable.d.ts" />
-/// <reference path="lib.d/node/node.d.ts" />
-/// <reference path="lib.d/underscore/underscore.d.ts" />
